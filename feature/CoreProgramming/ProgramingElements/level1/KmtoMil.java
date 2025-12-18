c73e1d4f321a0ae3d3b54ce536d69fd4c9903b0c
Murari <perumallamurari@gmail.com>
2025-12-18 21:59:18 +0530

    Clean main branch (remove project files)

--- feature/CoreProgramming/ProgramingElements/level1/KmtoMil.java
+++
@@ -1,5 +0,0 @@
-public class KmtoMil{
-    public static void main(String[] args){
-        System.out.println("The distance 10.8 Km in miles is "+ (10.8/1.6));
-    }
-}
--- feature/CoreProgramming/ProgramingElements/level1/SamMarks.java
+++
@@ -1,5 +0,0 @@
-public class SamMarks{
-    public static void main(String[] args){
-        System.out.println("Sam's average marks in PCM is "+ (94+95+96)/3);
-    }
-}
